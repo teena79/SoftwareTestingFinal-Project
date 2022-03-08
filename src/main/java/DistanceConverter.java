cd8a8d00248680a82c3b3c93800bfe05aaa4021a
vamsishree <vamsishree06@gmail.com>
2022-03-08 10:52:00 +0100

    Starting

---
+++ src/main/java/DistanceConverter.java
@@ -0,0 +1,11 @@
+public class DistanceConverter {
+    public int kms_to_meters(int kms) {
+        return kms * 1000;
+    }
+    public int kms_to_cms(int kms) {
+        return kms * 100000;
+    }
+    public int kms_to_mm(int kms){
+            return kms*1000000;
+        }
+    }
---
+++ src/test/java/DistanceConverterTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DistanceConverterTest {
+    @Test
+    public void kms_to_metres_test(){
+        DistanceConverter a = new DistanceConverter();
+        assertEquals(10000,a.kms_to_meters(10));
+    }
+    @Test
+    public void kms_to_cms_test(){
+        DistanceConverter a = new DistanceConverter();
+        assertEquals(1000000,a.kms_to_cms(10));
+    }
+    @Test
+    public void kms_to_mm_test(){
+        DistanceConverter a = new DistanceConverter();
+        assertEquals(10000000,a.kms_to_mm(10));
+    }
+}
